8dd7c6f1bde84dc13bab7a1d0b669799d646de5b
Natanael Lima <taelima1997@gmail.com>
2022-03-04 23:52:40 -0300

    add GestureHandlerRootView

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Camera from './components/Camera';
 
 const Main: React.FC = () => {
-  return <Camera />;
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Camera />
+    </GestureHandlerRootView>
+  );
 };
 
 export default Main;
